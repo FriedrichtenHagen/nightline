557635a1a547bd4e44144dd14b94bf9b5da3f726
Irakli Shubitidze <Irakli.Shubitidze@mdkk-f2b.local>
2022-11-07 19:18:05 +0100

    fixed addEventListener for sidebar, talwind bug, breakpoints in text, stichpunkte in imprint, linking to english sites

--- 0navmenue.js
+++ 0navmenue.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  const navLinks = document.querySelectorAll('.nav-links a');
 
   burger.addEventListener('click', () => {
 
@@ -15,10 +15,31 @@ const navSlide = () => {
       }
     });
     burger.classList.toggle('toggle');
-
   });
-
-
 }
+
+document.addEventListener('click', () => {
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelector('.nav-links');
+  const burger = document.querySelector('.burger');
+  const line1 = document.querySelector('.line1');
+  const line2 = document.querySelector('.line2');
+  const line3 = document.querySelector('.line3');
+
+  const shouldNavClose = !event.target.classList.contains(nav) 
+    && !event.target.contains(navLinks)
+    && !event.target.contains(line1)
+    && !event.target.contains(line2)
+    && !event.target.contains(line3)
+    && nav.classList.contains('nav-active');
+  const bodyWasClicked =
+    event.target.tagName === 'BODY'
+    && nav.classList.contains('nav-active');
+
+  if (shouldNavClose || bodyWasClicked) {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+  }
+})
 
 navSlide();
